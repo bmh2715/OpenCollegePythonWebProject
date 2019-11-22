67512af8d961dd547dd28e697d9e9eaaf7ba45c3
민혜 <bmh2715@naver.com>
2019-11-22 21:21:48 +0900

    forloop.py를 추가함

---
+++ oc_1122/dictionary1.py
@@ -0,0 +1,6 @@
+a={"맥북": 80, "ps4slim": 20, "desktopPC": 40}
+b=a.values()
+print(b)
+print(type(b))
+
+b=
---
+++ oc_1122/forloop.py
@@ -0,0 +1,14 @@
+a={'김도한': '010-7878-7878', '신봉건': '010-889-8989'}
+
+for name in a:
+    print(name)
+
+for key in a.keys():
+    print(key)
+
+for item in a.items():
+    print(item)
+
+a=range(10)
+for i in a:
+    print(i)
